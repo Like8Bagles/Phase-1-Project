41d3aaa529d5f1575af43e8270dc7c90d885707f
Anthony <anthonykmbanks@gmail.com>
2021-04-05 17:30:04 -0400

    preparing to append list with characters

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ document.addEventListener("DOMContentLoaded", () =>{
     getCharacters()
 })
 
+function fixName() {
+
+}
+
 function getCharacters() {
+    let ul = document.getElementById("character-list")
+    ul.innerHTML = ""
     fetch("https://api.jikan.moe/v3/anime/20/characters_staff")
     .then(res => res.json())
     .then(data => {
         console.log(data)
+        data.forEach(character => {
+            return ul.innerHTML += `
+
+            `
+        })
     })
 }
