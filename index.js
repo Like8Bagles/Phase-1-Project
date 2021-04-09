4fdc57e1d07d5917324412254893540720f51e07
Anthony <anthonykmbanks@gmail.com>
2021-04-09 18:54:04 -0400

    forgot to commit for a while but project complete

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
+const BASE_URL = "https://api.jikan.moe/v3/anime/20"
+let characters
 document.addEventListener("DOMContentLoaded", () =>{
     getCharacters()
 })
 
-function fixName() {
-
-}
-
 function getCharacters() {
-    let ul = document.getElementById("character-list")
-    ul.innerHTML = ""
-    fetch("https://api.jikan.moe/v3/anime/20/characters_staff")
+    let ul = document.getElementById("list");
+    ul.innerHTML = "";
+    fetch(BASE_URL + "/characters_staff")
     .then(res => res.json())
     .then(data => {
-        console.log(data)
-        data.characters.forEach(character => {
+        characters = data.characters
+        console.log(characters)
+        data.characters.forEach(character => {          
             ul.innerHTML += 
-            `<li>${character.name}</li>`
+            `<li><a href="#" id="${character.mal_id}">${fixName(character.name)}</a>  <button id = "button">Like!</button></li>`
         })
+        addEventListeners()
     })
 }
+
+function addEventListeners() {
+    let ul = document.getElementById("list");
+    ul.addEventListener("click", handleClick)
+}
+
+function handleClick(e) {
+    console.log(e)
+    let id = e.target.id
+    let main = document.getElementById("main-content")
+    let button = e.target
+    let div = document.getElementById("content")
+    
+    if (id === "button") {
+        button.style.backgroundColor = "#FF0000"
+    }
+    else {
+        const content = characters.find(character => e.target.id == character.mal_id).image_url
+        const role = characters.find(character => e.target.id == character.mal_id).role
+        const name = characters.find(character => e.target.id == character.mal_id).name
+        div.innerHTML = ""
+        div.innerHTML += `<img src = "${content}" alt = "${role}">
+        <h3>${fixName(name)}</h3>`
+    }
+}
+
+function fixName(name) {
+    if (name.includes(", ")) {
+        let splitName = name.split(", ");
+        let newName = splitName[1] + " " + splitName[0]
+        return newName
+    }
+    else {
+        return name
+    }
+}
+
